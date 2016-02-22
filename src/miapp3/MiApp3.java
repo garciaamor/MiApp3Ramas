99197bc73338ebdfcfb2707ff9cac985d61ca602
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-02-22 09:12:51 +0100

    Cambio dentro del bug

--- src/miapp3/MiApp3.java
+++ src/miapp3/MiApp3.java
@@ -5,6 +5,7 @@ public class MiApp3 {
     public static void main(String[] args) {
         //Primer commit dentro de la master
         //Cambio en la master antes del bug
+        //Cambio dentro de la rama bug
     }
     
 }
